1b563f0d6ecb8c71cca156ffe99e982b113a7130
Elsam Marura <elsam.marura@gmail.com>
2025-08-04 06:34:19 +0800

    AstagaSetang

--- Assets/FPSManager.cs
+++ Assets/FPSManager.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class FPSManager : MonoBehaviour
 {
-    void Awake()
-    {
-        // Hanya ada satu instance
-        if (FindObjectsByType<FPSManager>(FindObjectsSortMode.None).Length > 1)
-        {
-            Destroy(gameObject);
-            return;
-        }
+    //void Awake()
+    //{
+    //    // Hanya ada satu instance
+    //    if (FindObjectsByType<FPSManager>(FindObjectsSortMode.None).Length > 1)
+    //    {
+    //        Destroy(gameObject);
+    //        return;
+    //    }
 
-        DontDestroyOnLoad(gameObject); // Biar tidak hilang saat pindah scene
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
-    }
+    //    DontDestroyOnLoad(gameObject); // Biar tidak hilang saat pindah scene
+    //    QualitySettings.vSyncCount = 1;
+    //    //Application.targetFrameRate = 60;
+    //}
 }
